dd5871290e7cb71a7cc4572a6a73619f7e099cc7
Dineth0 <dinethnakandala10@gmail.com>
2026-02-28 14:23:50 +0530

    initial serviceregisry

---
+++ serviceregisry/src/main/java/lk/ijse/serviceregisry/ServiceregisryApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.serviceregisry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregisryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceregisryApplication.class, args);
+    }
+
+}
---
+++ serviceregisry/src/test/java/lk/ijse/serviceregisry/ServiceregisryApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.serviceregisry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregisryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
