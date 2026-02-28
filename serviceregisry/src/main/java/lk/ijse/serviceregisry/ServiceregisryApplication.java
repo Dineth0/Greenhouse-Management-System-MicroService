24bd292837ab632c2f554b0a8809d77fabf6cdc5
Dineth0 <dinethnakandala10@gmail.com>
2026-02-28 14:51:59 +0530

    connect service-registry through 8761

--- serviceregisry/src/main/java/lk/ijse/serviceregisry/ServiceregisryApplication.java
+++ serviceregisry/src/main/java/lk/ijse/serviceregisry/ServiceregisryApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.serviceregisry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceregisryApplication {
 
     public static void main(String[] args) {
