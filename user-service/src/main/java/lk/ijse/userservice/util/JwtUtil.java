eb5079642a2559640c8ea79f2ae72197d761c5b0
Dineth0 <dinethnakandala10@gmail.com>
2026-03-04 14:15:40 +0530

    create JwtUtil class for user-service and implement token

---
+++ user-service/src/main/java/lk/ijse/userservice/util/JwtUtil.java
@@ -0,0 +1,40 @@
+package lk.ijse.userservice.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.function.Function;
+
+public class JwtUtil {
+    private String SECRET = "mysecret1234###";
+
+    public String generateToken(String username) {
+        return Jwts.builder().setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256,SECRET).compact();
+    }
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody().getSubject();
+    }
+    public boolean validateToken(String token, String username) {
+        return (extractUsername(token).equals(username) && !isTokenExpired(token));
+    }
+    private boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+    private Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+
+    }
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimFromToken(token);
+         return claimsResolver.apply(claims);
+    }
+    private Claims getAllClaimFromToken(String token) {
+        return  Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+    }
+}
