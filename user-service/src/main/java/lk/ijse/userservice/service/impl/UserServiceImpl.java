571517e5b1346721ad387c84c0d78f60c261e251
Dineth0 <dinethnakandala10@gmail.com>
2026-03-04 15:50:41 +0530

    add findByUsername method for userserviceImpl

--- user-service/src/main/java/lk/ijse/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/lk/ijse/userservice/service/impl/UserServiceImpl.java
@@ -60,8 +60,8 @@ public class UserServiceImpl implements UserService {
                 getAuthority(user)
         );
     }
-
-
-
-
+    @Override
+    public User findByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
 }
