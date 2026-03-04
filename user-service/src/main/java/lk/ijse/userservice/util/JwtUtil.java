844294417229d91d5a737d31792563d65c04335d
Dineth0 <dinethnakandala10@gmail.com>
2026-03-04 15:45:57 +0530

    add JwtUtil for genetateToken

--- user-service/src/main/java/lk/ijse/userservice/util/JwtUtil.java
+++ user-service/src/main/java/lk/ijse/userservice/util/JwtUtil.java
@@ -3,15 +3,27 @@ package lk.ijse.userservice.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lk.ijse.userservice.dto.UserDTO;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class JwtUtil {
     private String SECRET = "mysecret1234###";
 
-    public String generateToken(String username) {
-        return Jwts.builder().setSubject(username)
+    public String generateToken(UserDTO userDTO) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", userDTO.getRole());
+        claims.put("id", userDTO.getId());
+        return doGenerateToken(claims, userDTO.getUsername());
+
+    }
+    public String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256,SECRET).compact();
