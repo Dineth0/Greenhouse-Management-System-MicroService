7152233f57a649a484b1ceafb8dcc6dea86ba33c
Dineth0 <dinethnakandala10@gmail.com>
2026-03-06 00:21:14 +0530

    add createZone method for ZoneService in zone-management-service

---
+++ Zone-Management-Service/src/main/java/lk/ijse/zonemanagementservice/service/ZoneService.java
@@ -0,0 +1,7 @@
+package lk.ijse.zonemanagementservice.service;
+
+import lk.ijse.zonemanagementservice.entity.Zone;
+
+public interface ZoneService {
+    Zone createZone(Zone zone);
+}
---
+++ Zone-Management-Service/src/main/java/lk/ijse/zonemanagementservice/service/impl/ZoneServiceImpl.java
@@ -0,0 +1,42 @@
+package lk.ijse.zonemanagementservice.service.impl;
+
+import lk.ijse.zonemanagementservice.dto.DeviceRequestDTO;
+import lk.ijse.zonemanagementservice.dto.DeviceResponseDTO;
+import lk.ijse.zonemanagementservice.dto.IOTAuthRequestDTO;
+import lk.ijse.zonemanagementservice.dto.IOTAuthResponseDTO;
+import lk.ijse.zonemanagementservice.entity.Zone;
+import lk.ijse.zonemanagementservice.repo.ZoneManagementRepo;
+import lk.ijse.zonemanagementservice.service.IotService;
+import lk.ijse.zonemanagementservice.service.ZoneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ZoneServiceImpl implements ZoneService {
+
+    private final IotService iotService;
+    private final ZoneManagementRepo zoneManagementRepo;
+
+    @Override
+    public Zone createZone(Zone zone) {
+
+        if(zone.getMinTemp() > zone.getMaxTemp()){
+            throw new RuntimeException("MinTemp must be less than MaxTemp");
+        }
+
+        IOTAuthRequestDTO authRequestDTO = new IOTAuthRequestDTO("user", "password");
+        iotService.registerUser(authRequestDTO).block();
+
+        IOTAuthResponseDTO authResponseDTO = iotService.loginUser(authRequestDTO).block();
+        String token = authResponseDTO.getAccessToken();
+
+        DeviceRequestDTO deviceRequestDTO = new DeviceRequestDTO(zone.getName(), "Zone-A");
+
+        DeviceResponseDTO deviceResponseDTO = iotService.registerDevice(token, deviceRequestDTO).block();
+
+        zone.setDeviceId(deviceResponseDTO.getDeviceId());
+
+        return zoneManagementRepo.save(zone);
+    }
+}
