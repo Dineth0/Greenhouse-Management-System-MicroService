1a012756c3ac022178006835affcd38ee840b5cc
Dineth0 <dinethnakandala10@gmail.com>
2026-03-05 21:43:20 +0530

    create DeviceRequestDTO class for zone-management service

--- Zone-Management-Service/src/main/java/lk/ijse/zonemanagementservice/dto/DeviceRequestDTO.java
+++ Zone-Management-Service/src/main/java/lk/ijse/zonemanagementservice/dto/DeviceRequestDTO.java
@@ -1,4 +1,13 @@
 package lk.ijse.zonemanagementservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class DeviceRequestDTO {
+    private String name;
+    private String zoneId;
 }
