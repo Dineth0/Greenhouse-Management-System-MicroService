bf263f53e639cdef46fca17e83f14030eb45bdf6
Dineth0 <dinethnakandala10@gmail.com>
2026-03-05 19:21:47 +0530

    establish communication between zone-management-service and config-server via eureka

---
+++ Zone-Management-Service/src/main/java/lk/ijse/zonemanagementservice/entity/Zone.java
@@ -0,0 +1,23 @@
+package lk.ijse.zonemanagementservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Zone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double maxTemp;
+    private double minTemp;
+    private String deviceId;
+}
