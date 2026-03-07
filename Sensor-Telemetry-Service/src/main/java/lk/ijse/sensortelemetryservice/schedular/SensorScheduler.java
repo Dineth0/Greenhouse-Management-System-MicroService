2f2bcd67078a93cbd1e06258166cfca3ddb09cf7
Dineth0 <dinethnakandala10@gmail.com>
2026-03-08 01:09:52 +0530

    add SensorScheduler

---
+++ Sensor-Telemetry-Service/src/main/java/lk/ijse/sensortelemetryservice/schedular/SensorScheduler.java
@@ -0,0 +1,31 @@
+package lk.ijse.sensortelemetryservice.schedular;
+
+import lk.ijse.sensortelemetryservice.dto.SensorDataDTO;
+import lk.ijse.sensortelemetryservice.service.SensorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
+@EnableScheduling
+public class SensorScheduler {
+    private final SensorService sensorService;
+    private final WebClient webClient;
+
+    private final String url = "http://localhost:8083/api/automation/data";
+    @Scheduled(fixedRate = 10000)
+    public void schedule() {
+        SensorDataDTO sensorDataDTO = sensorService.fetchSensorData();
+
+        webClient.post()
+                .uri(url)
+                .bodyValue(sensorDataDTO)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+    }
+}
