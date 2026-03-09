9847afea86942ab11c69d1e985cd0bfa284e756a
Dineth0 <dinethnakandala10@gmail.com>
2026-03-09 13:47:59 +0530

    initialize automation-control-service

---
+++ Automation-Control-Service/src/main/java/lk/ijse/automationcontrolservice/AutomationControlServiceApplication.java
@@ -0,0 +1,15 @@
+package lk.ijse.automationcontrolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AutomationControlServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationControlServiceApplication.class, args);
+	}
+
+}
