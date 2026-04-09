09de8580ddb2e1c101cb05fb0681ebd789103ed5
Dineth0 <dinethnakandala10@gmail.com>
2026-04-09 12:47:06 +0530

    create endpoint for register crop batch at Crop controller

---
+++ Crop-Inventory-Service/src/main/java/lk/ijse/cropinventoryservice/controller/CropController.java
@@ -0,0 +1,33 @@
+package lk.ijse.cropinventoryservice.controller;
+
+import lk.ijse.cropinventoryservice.dto.ResponseDTO;
+import lk.ijse.cropinventoryservice.entity.CropBatch;
+import lk.ijse.cropinventoryservice.service.CropService;
+import lk.ijse.cropinventoryservice.util.VarList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/crop")
+@RequiredArgsConstructor
+@CrossOrigin
+public class CropController {
+
+    private final CropService cropService;
+
+    @PostMapping
+    public ResponseEntity<ResponseDTO> registerCrop(@RequestBody CropBatch cropBatch) {
+        try {
+            CropBatch creatCropBatch = cropService.registerBatch(cropBatch);
+
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(new ResponseDTO(VarList.Created, "Crop Batch Created", creatCropBatch));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO(VarList.Internal_Server_Error, e.getMessage(), null));
+        }
+    }
+}
